8c02f825068bd122035bb18601b9b253da5be3ea
adithyab-20 <ab12095@nyu.edu>
2025-07-28 20:24:18 -0400

    fix: add nanobind dependency to vcpkg and resolve CI formatting issues

--- src/calculator/py_Calculator.cpp
+++ src/calculator/py_Calculator.cpp
@@ -6,18 +6,19 @@ namespace nb = nanobind;
 using namespace nb::literals;
 
 NB_MODULE(ab_calculator, m) {
-    m.doc() = "Python bindings for AB Calculator component";
-    
-        nb::class_<ab::calculator::Calculator>(m, "Calculator")
-        .def(nb::init<>(), "Default constructor")
-        .def("add", &ab::calculator::Calculator::add, 
-             "Add two integers", "lhs"_a, "rhs"_a)
-        .def("subtract", &ab::calculator::Calculator::subtract,
-             "Subtract second integer from first", "lhs"_a, "rhs"_a)
-        .def("multiply", &ab::calculator::Calculator::multiply,
-             "Multiply two integers", "lhs"_a, "rhs"_a)
-        .def("divide", &ab::calculator::Calculator::divide,
-             "Divide first integer by second (throws on division by zero)", "lhs"_a, "rhs"_a);
+  m.doc() = "Python bindings for AB Calculator component";
 
-    nb::exception<std::invalid_argument>(m, "InvalidArgument");
+  nb::class_<ab::calculator::Calculator>(m, "Calculator")
+      .def(nb::init<>(), "Default constructor")
+      .def("add", &ab::calculator::Calculator::add, "Add two integers", "lhs"_a,
+           "rhs"_a)
+      .def("subtract", &ab::calculator::Calculator::subtract,
+           "Subtract second integer from first", "lhs"_a, "rhs"_a)
+      .def("multiply", &ab::calculator::Calculator::multiply,
+           "Multiply two integers", "lhs"_a, "rhs"_a)
+      .def("divide", &ab::calculator::Calculator::divide,
+           "Divide first integer by second (throws on division by zero)",
+           "lhs"_a, "rhs"_a);
+
+  nb::exception<std::invalid_argument>(m, "InvalidArgument");
 }
